0ceb747c28d7836023cf19a7634d9787d7cbc64e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-10 13:20:58 -0300

    arrumando input

--- jogo_memoria.py
+++ jogo_memoria.py
@@ -1,8 +1,12 @@
+def main():
+    if mostrar_menu():
+        ...
+
 def mostrar_menu():
     while True:
         print("Bem-vindo ao jogo da memória")
         try:
-            escolha = int(input("Qual opção você deseja?\n1. Jogar\n3. Sair"))
+            escolha = int(input("Qual opção você deseja?\n1. Jogar\n2. Sair"))
             if escolha == 1:
                 return True
             elif escolha == 2:
@@ -11,3 +15,5 @@ def mostrar_menu():
                 print("Essa opção não existe.")
         except ValueError:
             print("Você digitou algo errado.")
+
+main()
