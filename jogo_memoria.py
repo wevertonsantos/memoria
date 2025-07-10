ea0f79a80ceb891ace340d7e0c0155bb97bd1545
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-10 17:19:31 -0300

    ajustando código.

--- jogo_memoria.py
+++ jogo_memoria.py
@@ -1,16 +1,18 @@
 import random
 
 def main():
-    reveladas = []
     
     if mostrar_menu():
         pares = criar_pares()
         cartas = montar_cartas(pares)
         cartas_embaralhadas = embaralhar_cartas(cartas)
+        reveladas = []
         mostrar_tabulheiro(cartas_embaralhadas,reveladas)
+
         pos1,pos2 = escolher_cartas(cartas)
         reveladas.append(pos1)
         reveladas.append(pos2)
+        
         mostrar_tabulheiro(cartas_embaralhadas,reveladas)
         verificar_par(cartas,reveladas)
 
@@ -62,14 +64,18 @@ def escolher_cartas(cartas):
         while True:
             pos1 = int(input("Escolha a primeira posição: "))
             pos2 = int(input("Escolha a segunda posição: "))
-            if pos1 and pos2 > len(cartas) - 1 or pos1 and pos2 < 0:
+            if pos1 < 0 or pos2 < 0 or pos1 >= len(cartas) or pos2 >= len(cartas):
                 print("Essa opção não existe")
+            elif pos1 == pos2:
+                print("Você não pode escolher a mesma posição duas vezes")
             else:
                 return pos1,pos2
     except ValueError:
         print("Você escolheu algo errado!")
 
 def verificar_par(cartas,reveladas):
-    ...
+    for i in range(len(cartas)):
+        print(cartas[i])
+        print(reveladas)
 
 main()
